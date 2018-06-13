e9e3bc3abdd5a1ceb4444f94c2f11788cc25703a
Tom Ploetz <ploetztj@miamioh.edu>
2018-06-13 09:42:11 -0400

    intial commit

---
+++ command-line-input-exercises-pairs/MakeChange/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeChange
+{
+    class Program
+    {
+        /*
+        Write a command line program which prompts the user for the total bill, and the amount tendered. It should then display the change required.
+ 
+        C:\Users> MakeChange
+         
+        Please enter the amount of the bill: 23.65
+        Please enter the amount tendered: 100.00
+        The change required is 76.35
+        */
+        static void Main(string[] args)
+        {
+			
+			Console.WriteLine("Please enter the amount of the bill: ");
+			string billValue = Console.ReadLine();
+			double billAmount = double.Parse(billValue);
+
+			Console.WriteLine("Please enter the amount tendered: ");
+			string tenderValue = Console.ReadLine();
+			double tenderAmount = double.Parse(tenderValue);
+
+			double change = tenderAmount - billAmount;
+
+			Console.WriteLine("The change required is $" + change);
+
+			Console.ReadKey();
+			
+        }
+    }
+}
---
+++ command-line-input-exercises-pairs/MartianWeight/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianWeight
+{
+	class Program
+	{
+		/*
+        In case you've ever pondered how much you weight on Mars, here's the calculation:
+ 	    Wm = We* 0.378
+ 	    where 'Wm' is the weight on Mars, and 'We' is the weight on Earth
+
+        Write a command line program which accepts a series of Earth weights from the user  
+        and displays each Earth weight as itself, and its Martian equivalent.
+
+
+        C:\Users> MartianWeight  
+        Enter a series of Earth weights (space-separated): 98 235 185
+            
+        98 lbs.on Earth, is 37 lbs.on Mars.
+        235 lbs.on Earth, is 88 lbs.on Mars.
+        185 lbs.on Earth, is 69 lbs.on Mars. 
+        */
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your weight on Earth in pounds: ");
+			string userWeight = Console.ReadLine();
+
+			char[] splitWeight = { ' ' };
+			string[] weight = userWeight.Split(splitWeight);
+			double earthWeight = 0;
+			int marsWeightWhole = 0;
+
+			for (int i = 0; i < weight.Length; i++)
+			{
+				earthWeight = double.Parse(weight[i]);
+
+				double marsWeight = 0;
+				marsWeight = earthWeight * 0.378;
+				marsWeightWhole = (int)marsWeight;
+
+				Console.WriteLine(weight[i] + "lbs. on Earth, is " + marsWeightWhole + "lbs. on Mars.");
+			}
+
+			Console.ReadKey();
+
+		}
+	}
+}
